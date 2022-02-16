e702ac85c31e8eb5dd1311a3d05d24eede9c54fc
JohnCru1976 <94476964+JohnCru1976@users.noreply.github.com>
2022-02-16 14:32:56 +0100

    Update

--- 05-Lambda/lambdaEx.hs
+++ 05-Lambda/lambdaEx.hs
@@ -13,3 +13,4 @@ lambdaVersion x y = (\sumSquare squareSum ->
                     if sumSquare > squareSum
                     then sumSquare
                     else squareSum) (x^2 + y^2) ((x+y)^2)
+                    
