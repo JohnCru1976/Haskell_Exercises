145587ea617ffb3d20f8512e1d6524be35338ca9
JohnCru1976 <94476964+JohnCru1976@users.noreply.github.com>
2022-02-18 21:22:41 +0100

    Update

--- 03-Where/where.hs
+++ 03-Where/where.hs
@@ -1,9 +1,14 @@
 main :: IO()
 main = do    
-    print(calcChange 100 200)
     print(calcChange 200 100)
+    print(lambdaCalChange 100 200)
 
 calcChange owed given = if change  > 0
                         then change 
                         else 0
     where change = given - owed
+
+lambdaCalChange = (\owed given ->
+                    if given - owed > 0
+                    then given - owed
+                    else 0)
