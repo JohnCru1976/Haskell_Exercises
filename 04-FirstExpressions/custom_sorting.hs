2acd2e769ff249c2f2414e17f40925bf7fcdb291
JohnCru1976 <94476964+JohnCru1976@users.noreply.github.com>
2022-02-19 16:54:05 +0100

    sorting the elements of an array of strings

--- 04-FirstExpressions/custom_sorting.hs
+++ 04-FirstExpressions/custom_sorting.hs
@@ -5,19 +5,25 @@ main :: IO()
 
 main = do
     print(sort names) -- We use SORT to sort by the first element of each tuple
-    -- Output: [("Bernard","Sumner"),("Ian","Curtis"),("Peter","Hook"),("Stephen","Morris")]
+    -- [("Bernard","Sumner"),("Ian","Curtis"),("Peter","Hook"),("Stephen","Morris"),("Vicent","Sumner")]
     print(sortBy compareLastNames names) -- We use SORTBY to sort according to a passed function
-    -- Output: [("Ian","Curtis"),("Peter","Hook"),("Stephen","Morris"),("Bernard","Sumner")]
+    -- [("Ian","Curtis"),("Peter","Hook"),("Stephen","Morris"),("Bernard","Sumner"),("Vicent","Sumner")]
 
 
 -- This is an array of tuples we are sorting
-names = [("Ian","Curtis"),("Bernard","Sumner"),("Peter","Hook"),("Stephen","Morris")]
+names = [("Ian","Curtis"),("Bernard","Sumner"), ("Vicent","Sumner"),("Peter","Hook"),("Stephen","Morris")]
 
 -- This is a function to sort by last names
 compareLastNames name1 name2 = if lastName1 > lastName2
                               then GT      -- Greater Than
                               else if lastName1 < lastName2 -- else if
                                   then LT  -- Less Than
-                                  else EQ  -- Equal
+                                  else if firstName1 > firstName2
+                                      then GT 
+                                      else if firstName1 < firstName2
+                                         then LT
+                                         else EQ  -- Equal
   where lastName1 = snd name1
-        lastName2 = snd name2                                      
+        lastName2 = snd name2 
+        firstName1 = fst name1
+        firstName2 = fst name2                                     
