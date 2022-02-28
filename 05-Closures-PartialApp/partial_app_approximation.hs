da635a65ef7197e6d0168d43b6fe59c788e87e97
JohnCru1976 <94476964+JohnCru1976@users.noreply.github.com>
2022-03-01 00:44:13 +0100

    Partial Applications

--- 05-Closures-PartialApp/partial_app_approximation.hs
+++ 05-Closures-PartialApp/partial_app_approximation.hs
@@ -6,7 +6,8 @@ main::IO()
 main = do
     print(test_v1 4)
     print(test_v2 8)
-    print(test_v2 12)
+    print(test_v3 12)
+    print(test_v4 25)
 
 -- Without partial application. We need a middle function
 binaryPartialApplication f x = (\y-> f x y)
@@ -15,3 +16,4 @@ test_v1 = binaryPartialApplication (+) 2
 --With partial application. We can omit binaryPartialApplication
 test_v2 = (+) 2
 test_v3 = (2 +)
+test_v4 = (/5)
