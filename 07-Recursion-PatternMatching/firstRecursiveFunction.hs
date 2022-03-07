c6595ead1569ee9d277c72f068602dcad32d09a1
JohnCru1976 <juanbcru@gmail.com>
2022-03-07 10:16:42 +0100

    Modifing recursive fibonacci

--- 07-Recursion-PatternMatching/firstRecursiveFunction.hs
+++ 07-Recursion-PatternMatching/firstRecursiveFunction.hs
@@ -3,13 +3,8 @@ main = do
     print(myGCD 20 16)
     print(multiply 5 25)
     print(factorial 5)
-    print(fibonacci 1) -- 1 
-    print(fibonacci 2) -- 1 
-    print(fibonacci 3) -- 2
-    print(fibonacci 4) -- 3
-    print(fibonacci 5) -- 5
-    print(fibonacci 6) -- 8
-    print(fibonacci 7) -- 13
+    print(map fibonacci [1..10])
+    
 
 -- Example of a recursion function
 -- Euclid's algorithm - Greatest Common Divisor
@@ -32,5 +27,4 @@ factorial a = if a <= 0
 -- Personal example: fibonacci
 fibonacci a
   | a <= 2 = 1
-  | a == 1 = 0
   | otherwise = fibonacci (a-1) + fibonacci (a-2)
