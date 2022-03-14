53411a855d5ac6cfc81d76517f25c7cc67e923f7
JohnCru1976 <juanbcru@gmail.com>
2022-03-14 20:54:09 +0100

    Exercises update

--- 09-Higher-Order-Functions.hs/exercises.hs
+++ 09-Higher-Order-Functions.hs/exercises.hs
@@ -1,10 +1,10 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# HLINT ignore "Use any" #-}
 {-# HLINT ignore "Use null" #-}
+{-# HLINT ignore "Use any" #-}
 main::IO()
 main = do
-    print ()
+    print (myElem 4 [1,2,3,4])
 
 -- Q9.1 Use filter and length to re-create the elem function
-myElem x xs = not (length xs == 0 || filter x xs /= [])
+myElem a xs = not (length xs == 0 || filter (== a) xs /= [])
               
